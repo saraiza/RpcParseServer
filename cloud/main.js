eecf7962802f1a200da0585009531bf60efa31d5
Steve Araiza <saraiza@908devices.com>
2020-04-05 16:07:29 -0400

    working game resolution

--- cloud/main.js
+++ cloud/main.js
@@ -20,15 +20,85 @@ Parse.Cloud.afterSave("User", (request) => {
 
 Parse.Cloud.afterSave("GameV1", (request) => {
   console.log("GameV1 afterSave function triggered")
-  /*
-  const query = new Parse.Query("Post");
-  query.get(request.object.get("post").id)
-    .then(function(post) {
-      post.increment("comments");
-      return post.save();
-    })
-    .catch(function(error) {
-      console.error("Got an error " + error.code + " : " + error.message);
-    });
-    */
+
+  var FooBar = Parse.Object.extend("FooBar");
+  var fooBar = new FooBar();
+  fooBar.destroy().then((foos)=> {
+    console.log("foos d")
+  })
+
+  
+  const query = new Parse.Query("GameV1");
+  
+  // 
+  const results = query.find().then(function(results) {
+    let map = new Map()
+    var len = results.length
+    for(let i =0; i < results.length; ++i) {
+      var row = results[i]
+      var usr = row.get("username")
+      if(""==usr)
+        continue
+      var choice = row.get("choice")
+      console.log("row: usr=" + usr + " choice=" + choice)
+      map.set(usr, choice)
+      
+      // Do we have choices from two unique users? (hacky)
+      if(map.size != 2) 
+        continue // not yet
+        
+      // Yes, we may complete the game
+      var usr1, choice1, usr2, choice2
+      var k = 0;
+      for(let item of map[Symbol.iterator]()) {
+        ++k
+          if(1 == k) {
+            usr1 = item[0]
+            choice1 = item[1]
+          }
+          else {
+            usr2 = item[0]
+            choice2 = item[1]
+          }
+      }
+      
+      // Resolve the game logic
+      var winner
+      if(choice1=="rock" && choice2=="rock")
+        winner = "draw"
+      else if(choice1=="rock" && choice2=="paper")
+        winner = usr2
+      else if(choice1=="rock" && choice2=="scissors")
+        winner = usr1
+      else if(choice1=="paper" && choice2=="rock")
+        winner = usr1
+      else if(choice1=="paper" && choice2=="paper")
+        winner = "draw"
+      else if(choice1=="paper" && choice2=="scissors")
+        winner = usr2
+      else if(choice1=="scissors" && choice2=="rock")
+        winner = usr2
+      else if(choice1=="scissors" && choice2=="paper")
+        winner = usr1
+      else if(choice1=="scissors" && choice2=="scissors")
+        winner = "draw"
+
+      console.log("winner: " + winner)
+      // Remove all the entries from the table
+      // Add one entry with the result
+      
+      var Game = Parse.Object.extend("Winner");
+      var game = new Game();
+      game.set("username", winner)
+      game.save()
+
+      return;
+    }
+  })
+  .catch(function(error) {
+    console.error("Got an error " + error.code + " : " + error.message);
+  });
+
+  
+    
 });
